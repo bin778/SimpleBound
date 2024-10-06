142e14f165cecbbfcb57fcbb64a03ae2c00c320d
최영빈 <bin778@naver.com>
2024-10-06 09:53:30 +0900

    feat stage

--- MyApp/view/GameScreen.tsx
+++ MyApp/view/GameScreen.tsx
@@ -33,9 +33,15 @@ const mapMatrix = [
 
 const GameScreen: React.FC<GameScreenProps> = ({ navigation }) => {
   const [score, setScore] = useState(0);
+  const [stage, setStage] = useState(1);
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [playerPosition, setPlayerPosition] = useState(initialPlayerPosition);
   const [bombs, setBombs] = useState<{ row: number, col: number, explode: boolean, affectedTiles?: { row: number, col: number }[] }[]>([]);
+
+  // 피격 시간, 폭발 시간, 폭탄 생성 시간
+  const [explodeHit, setExplodeHit] = useState(500);
+  const [explodeTime, setExplodeTime] = useState(2000);
+  const [creationTime, setCreationTime] = useState(2000);
 
   // 플레이어 이동 함수
   const movePlayer = (direction: string) => {
@@ -89,10 +95,35 @@ const GameScreen: React.FC<GameScreenProps> = ({ navigation }) => {
 
         setTimeout(() => {
           setBombs((prevBombs) => prevBombs.slice(1)); // 폭탄 제거
-        }, 500); // 폭발 이미지 0.5초 동안 유지
-      }, 2000); // 2초 후 폭발
-    }, 2000); // 2초 마다 새로운 폭탄 생성
+        }, explodeHit); // 플레이어가 닿으면 피격
+      }, explodeTime); // 폭탄 생성 후 폭발
+    }, creationTime); // 새로운 폭탄 생성
   };
+
+  useEffect(() => {
+    if (score >= 150 && stage === 1) { // 2스테이지
+      setCreationTime(1000);
+      setStage(2);
+    } else if (score >= 300 && stage === 2) { // 3스테이지
+      setExplodeHit(750);
+      setCreationTime(700);
+      setStage(3);
+    } else if (score >= 450 && stage === 3) { // 4 스테이지
+      setCreationTime(500);
+      setStage(4);
+    } else if (score >= 600 && stage === 4) { // 5 스테이지
+      setExplodeHit(1000);
+      setExplodeTime(1000);
+      setStage(5);
+    } else if (score >= 750 && stage === 5) { // 6 스테이지
+      setCreationTime(250);
+      setStage(6);
+    } else if (score >= 900 && stage === 6) { // 7 스테이지
+      setExplodeHit(1200);
+      setCreationTime(150);
+      setStage(7);
+    }
+  }, [score, stage]);
 
   // 점수 및 폭발 처리 이펙트 구현
   useEffect(() => {
@@ -108,11 +139,23 @@ const GameScreen: React.FC<GameScreenProps> = ({ navigation }) => {
       if (scoreInterval) clearInterval(scoreInterval);
       if (bombInterval) clearInterval(bombInterval);
     };
-  }, [isGameStarted]);
+  }, [isGameStarted, explodeHit, explodeTime, creationTime]);
 
   const startGame = () => {
     setIsGameStarted(true);
   };
+
+  // 스테이지 리셋
+  const StageReset = () => {
+    setScore(0);
+    setStage(1);
+    setExplodeHit(500);
+    setExplodeTime(2000);
+    setCreationTime(2000);
+    setPlayerPosition(initialPlayerPosition);
+    setBombs([]);
+    setIsGameStarted(false);
+  }
 
   // 플레이어 피격 처리 이펙트 구현
   useEffect(() => {
@@ -123,10 +166,7 @@ const GameScreen: React.FC<GameScreenProps> = ({ navigation }) => {
 
     // 폭발 범위에 있으면 게임 오버 처리
     if (isPlayerHit) {
-      setIsGameStarted(false);
-      setScore(0);
-      setPlayerPosition(initialPlayerPosition);
-      setBombs([]);
+      StageReset();
       navigation.navigate('Result');
     }
   }, [bombs, playerPosition])
@@ -138,10 +178,7 @@ const GameScreen: React.FC<GameScreenProps> = ({ navigation }) => {
         <TouchableOpacity
           style={styles.arrowButton}
           onPress={() => {
-            setScore(0);
-            setPlayerPosition(initialPlayerPosition);
-            setBombs([]);
-            setIsGameStarted(false);
+            StageReset();
             navigation.navigate('Home');
           }}
         >
